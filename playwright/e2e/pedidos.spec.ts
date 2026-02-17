fed28b106f58358c8ba76d0d767ada9984ca50a5
Debiny <debiny.rocha@gmail>
2026-02-17 11:55:10 -0300

    consulta pedidos

---
+++ playwright/e2e/pedidos.spec.ts
@@ -0,0 +1,21 @@
+import { test, expect } from '@playwright/test'
+
+test('deve consultar um pedido aprovado', async ({ page }) => {
+  await page.goto('http://localhost:5173/')
+
+  //checkpoint 1: verificar se a página está online
+  await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint')
+
+  //checkpoint 2: navegar até a página de consulta de pedidos
+  await page.getByRole('link', { name: 'Consultar Pedido' }).click()
+  await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
+
+  await page.getByTestId('search-order-id').fill('VLO-PKAFMV')
+
+  await page.getByTestId('search-order-button').click()
+  await expect(page.getByTestId('order-result-id')).toBeVisible()
+  await expect(page.getByTestId('order-result-id')).toContainText('VLO-PKAFMV')
+  await expect(page.getByTestId('order-result-status')).toBeVisible()
+  await expect(page.getByTestId('order-result-status')).toContainText('APROVADO')
+
+})
