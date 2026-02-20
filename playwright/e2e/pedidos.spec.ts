7b99ffe985e9f53edf7d8ccf16dd0efe4bf3a031
Debiny <debiny.rocha@gmail>
2026-02-19 22:04:07 -0300

    Assert snapshot do playwright

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -4,6 +4,11 @@ import { TIMEOUT } from 'dns'
 ///AAA - Arrange, Act, Assert
 
 test('deve consultar um pedido aprovado', async ({ page }) => {
+
+    //Test Data
+
+    const order= 'VLO-PKAFMV'
+
   //Arrange
   await page.goto('http://localhost:5173/')
   await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint')
@@ -12,7 +17,7 @@ test('deve consultar um pedido aprovado', async ({ page }) => {
 
   //Act
   //await page.locator('//label[text()="Número do Pedido"]/..//input').fill('VLO-PKAFMV')
-  await page.getByRole('textbox', { name: 'Número do Pedido' }).fill('VLO-PKAFMV')
+  await page.getByRole('textbox', { name: 'Número do Pedido' }).fill(order)
   //await page.getByLabel('Número do Pedido').fill('VLO-PKAFMV');
   //await page.getByPlaceholder('Ex: VLO-ABC123').fill('VLO-PKAFMV')
 
@@ -25,10 +30,49 @@ test('deve consultar um pedido aprovado', async ({ page }) => {
  const containerPedido = page.getByRole('paragraph')
  .filter({ hasText: /^Pedido$/}) // = (/^ começa com) ($/ Termina com)Expresão regular para melhorar o criterio de filtragem
  .locator('..')//sobe para o elemento pai (a div que agrupa ambos)
- await expect(containerPedido).toContainText('VLO-PKAFMV', {timeout: 10_000})
+ await expect(containerPedido).toContainText('order', {timeout: 10_000})
 
  await expect(page.getByText('APROVADO')).toBeVisible({timeout: 30_000})
 
 
 
 })
+
+test('deve exibir mensagem quando o pedido não é encontrado',async ({page})=>{
+    
+const order = 'VLO-abc123'
+
+await page.goto('http://localhost:5173/')
+await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint')
+
+await page.getByRole('link', { name: 'Consultar Pedido' }).click()
+await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
+
+await page.getByRole('textbox', { name: 'Número do Pedido' }).fill(order)
+await page.getByRole('button', { name: 'Buscar Pedido' }).click()
+
+//await expect(page.locator('#root')).toContainText('Verifique o número do pedido e tente novamente');
+//await expect(page.locator('#root')).toContainText('Pedido não encontrado')
+
+// const title = page.getByRole('heading', {name: 'Pedido não encontrado'}, )
+// await expect(title).toBeVisible()
+  
+//buscando o elemnento atraves do xptah
+//const message = page.locator('//p[text()="Verifique o número do pedido e tente novamente"]') 
+
+//buscando o elemento atraves do local locator de modo mais simples
+// const message = page.locator('p', {hasText: 'Verifique o número do pedido e tente novamente'})
+// await expect(message).toBeVisible()
+
+//buscando o elemento usando o Assert snapshot do playwright
+test('test', async ({ page }) => {
+    await page.goto('http://localhost:5173/');
+    await expect(page.locator('#root')).toMatchAriaSnapshot(`
+      - img
+      - heading "Pedido não encontrado" [level=3]
+      - paragraph: Verifique o número do pedido e tente novamente
+      `);
+  });
+}
+
+)
